0535b3938f9b53977712b4520981104b51dbcf96
S-Cybulski <szymcyb04@protonmail.com>
2023-08-31 20:01:04 +0100

    Added getComputerChoice function
    chooses a random value of rock paper or scissors

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 let random = () => Math.floor(Math.random() * 3);
 
-console.log(random());
+function getComputerChoice(){
+    let choice = random();
+
+    switch(choice) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
