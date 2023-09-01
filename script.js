ba43fdc0728fbec4b1d66097d33cde40dd55095f
S-Cybulski <szymcyb04@protonmail.com>
2023-09-01 16:24:25 +0100

    Added playGame function to script.js
    Allows the player to play a single round of rock paper scissors requires
    player and computer input

--- script.js
+++ script.js
@@ -1,15 +1,51 @@
 let random = () => Math.floor(Math.random() * 3);
 
+let capitalise = (gameObject) => gameObject.charAt(0).toUpperCase() + gameObject.slice(1);
+
 function getComputerChoice(){
     let choice = random();
 
     switch(choice) {
         case 0:
-            return "rock";
+            return "Rock";
         case 1:
-            return "paper";
+            return "Paper";
         case 2:
-            return "scissors";
+            return "Scissors";
+    }
+}
+
+function playGame(playerSelection, computerSelection){
+    
+    playerSelection = capitalise(playerSelection)
+
+    switch (playerSelection | computerSelection){
+        case playerSelection == computerSelection:
+            return "Tie!"
+
+        case playerSelection == "Rock":
+            if (computerSelection == "Paper"){
+                return "You lose! Paper beats Rock."
+            }
+            else {
+                return "You win! Rock beats Scissors."
+            }
+
+        case playerSelection == "Paper":
+            if (computerSelection == "Scissors"){
+                return "You lose! Scissors beats Paper."
+            }
+            else {
+                return "You win! Paper beats Rock."
+            }
+
+        case playerSelection == "Scissors":
+            if (computerSelection == "Rock"){
+                return "You lose! Rock beats Scissors."
+            }
+            else {
+                return "You win! Scissors beats Paper."
+            }
     }
 }
 
