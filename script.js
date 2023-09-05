e866a017ba2e5210c773b4bd6af4802dcb0dbb86
S-Cybulski <szymcyb04@protonmail.com>
2023-09-05 13:05:55 +0100

    added game function to script.js and added scoring
    game function plays the game until someone reaches 5 points

--- script.js
+++ script.js
@@ -25,31 +25,53 @@ function playRound(playerSelection, computerSelection){
 
         case "Rock":
             if (computerSelection == "Paper"){
+                computerScore ++;
                 return "You lose! Paper beats Rock.";
             }
             else {
+                playerScore ++;
                 return "You win! Rock beats Scissors.";
             }
 
         case "Paper":
             if (computerSelection == "Scissors"){
+                computerScore ++;
                 return "You lose! Scissors beats Paper.";
             }
             else {
+                playerScore ++;
                 return "You win! Paper beats Rock.";
             }
 
         case "Scissors":
             if (computerSelection == "Rock"){
+                computerScore ++;
                 return "You lose! Rock beats Scissors.";
             }
             else {
+                playerScore ++;
                 return "You win! Scissors beats Paper.";
             }
     }
 }
 
+function game(){
+    while (computerScore < 5 && playerScore < 5){
+        let playerSelection = prompt("Rock, Paper, Scissors?");
+        let computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+    if (computerScore === 5){
+        console.log("Computer Wins!")
+    }
+
+    else {
+        console.log("Player Wins!")
+    }
+}
+
+let computerScore = 0;
+let playerScore = 0;
+
+game();
